dc496b50d659f914341a244bde943f770f7a224a
juansebastian458 <juansebastiian0116@gmail.com>
2025-08-08 22:38:03 -0500

    menu code

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+print("=== CALCULADORA ===")
+print("Selecciona una operación:")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicación")
+print("4. División")
+
+opcion = input("Ingresa el número de la operación (1/2/3/4): ")
